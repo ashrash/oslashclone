c68aaa7189e95d237593ef58a220dd739a78e3cf
Ashwin R <vivekashwin@gmail.com>
2022-10-15 12:44:12 +0530

    Modified mysql table schemas to accomodate shortlink as PK

--- DDL.sql
+++ DDL.sql
@@ -1,42 +1,40 @@
 -- DB DDL script for OSlash clone
-
 CREATE TABLE `users`
 (
- `id`           bigint NOT NULL AUTO_INCREMENT,
  `email`        varchar(100) NOT NULL ,
  `name`         varchar(100) NOT NULL ,
  `password`     varchar(100) NOT NULL ,
  `created_dttm` datetime NOT NULL ,
  `updated_dttm` datetime NOT NULL ,
 
-PRIMARY KEY (`id`)
+PRIMARY KEY (`email`)
 );
 
 
 CREATE TABLE `shortcut`
 (
- `id`           bigint NOT NULL ,
- `short_link`   varchar(60) NOT NULL ,
- `user_id`      bigint NOT NULL ,
+ `short_link` varchar(60) NOT NULL ,
+ `email_id`     varchar(100) NOT NULL ,
  `description`  varchar(250) NOT NULL ,
  `created_dttm` datetime NOT NULL ,
  `update_dttm`  datetime NOT NULL ,
 
-PRIMARY KEY (`id`),
-KEY `FK_2` (`user_id`),
-CONSTRAINT `FK_1` FOREIGN KEY `FK_2` (`user_id`) REFERENCES `user` (`id`)
+PRIMARY KEY (`short_link`),
+KEY `FK_2` (`email_id`),
+CONSTRAINT `FK_1` FOREIGN KEY `FK_2` (`email_id`) REFERENCES `users` (`email`)
 );
+
 
 
 CREATE TABLE `tags`
 (
  `id`           bigint NOT NULL ,
- `shortcut_id`  bigint NOT NULL ,
+ `short_link_id` varchar(60) NOT NULL ,
  `tag`          varchar(45) NOT NULL ,
  `created_dttm` datetime NOT NULL ,
  `updated_dttm` datetime NOT NULL ,
 
 PRIMARY KEY (`id`),
-KEY `FK_2` (`shortcut_id`),
-CONSTRAINT `FK_2` FOREIGN KEY `FK_2` (`shortcut_id`) REFERENCES `shortcut` (`id`)
+KEY `FK_2` (`short_link_id`),
+CONSTRAINT `FK_3` FOREIGN KEY `FK_2` (`short_link_id`) REFERENCES `shortcut` (`short_link`)
 );
